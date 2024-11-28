dd9cf9045576440794a6263ddcc298204dd733e6
jhPark <jonghyeon0780@gmail.com>
2024-11-28 23:16:43 +0900

    bootstrap 추가

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import React, {useEffect,useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [first, setFirst] = useState([]);
--- src/main/java/com/src/reacttest/controller/FirstController.java
+++ src/main/java/com/src/reacttest/controller/FirstController.java
@@ -3,7 +3,6 @@ package com.src.reacttest.controller;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
